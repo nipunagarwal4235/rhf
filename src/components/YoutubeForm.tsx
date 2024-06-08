eec592f246a851011055b3f3991dfcb39b7f880c
nipunagarwal4235 <nipunagarwal2002@gmail.com>
2024-06-09 01:18:47 +0530

    async validation

--- src/components/YoutubeForm.tsx
+++ src/components/YoutubeForm.tsx
@@ -146,6 +146,13 @@ const YoutubeForm = () => {
                     "Enter a different email address"
                   );
                 },
+                emailAvailable: async (fieldValue) => {
+                  const response = await fetch(
+                    `https://jsonplaceholder.typicode.com/users?email=${fieldValue}`
+                  );
+                  const data = await response.json();
+                  return data.length === 0 || "Enter a different email address";
+                },
               },
             })}
           />
