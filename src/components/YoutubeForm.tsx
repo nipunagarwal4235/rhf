d61c26084c6e94d92914db36ca07d8522157ff17
nipunagarwal4235 <nipunagarwal2002@gmail.com>
2024-06-08 22:34:19 +0530

    default values

--- src/components/YoutubeForm.tsx
+++ src/components/YoutubeForm.tsx
@@ -8,7 +8,19 @@ type FormData = {
 };
 
 const YoutubeForm = () => {
-  const form = useForm<FormData>();
+  const form = useForm<FormData>({
+    defaultValues: async () => {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/users/1"
+      );
+      const data = await response.json();
+      return {
+        username: data.username,
+        email: data.email,
+        channel: "",
+      };
+    },
+  });
   const { register, control, handleSubmit, formState } = form;
   const { errors } = formState;
 
